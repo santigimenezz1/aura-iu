de94388dfeb91f77af7516196cd64270b38bd506
santiago <santiago.manuel.gimenez97@gmail.com>
2024-04-26 16:44:34 +0200

    estilos botonToggle solucionado

--- src/components/Playground/BotonToggle/BotonToggle.jsx
+++ src/components/Playground/BotonToggle/BotonToggle.jsx
@@ -8,9 +8,11 @@ const ButonToggle = ({text, img, focus}) => {
 <button className={focus ? "cssbuttons-io" : "cssbuttons-io-on"}>
   <span>
     <div className='container__logo__titulo'>
-    <img className='logo' src={img}></img>
+    <img className='logo__crear' src={img}></img>
     </div>
+    <div style={{color:"white"}}>
       {text}
+    </div>
     </span>
 </button>
 
